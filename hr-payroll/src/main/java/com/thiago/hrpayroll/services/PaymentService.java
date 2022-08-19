fa35812abe3f96aecd68a0707bb7863ea4e1a506
Thiago Ferreira <tlrferreira83@gmail.com>
2022-08-19 06:10:45 -0300

    Feing Client

--- hr-payroll/src/main/java/com/thiago/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/com/thiago/hrpayroll/services/PaymentService.java
@@ -2,28 +2,18 @@ package com.thiago.hrpayroll.services;
 
 import com.thiago.hrpayroll.entities.Payment;
 import com.thiago.hrpayroll.entities.Worker;
+import com.thiago.hrpayroll.feingclients.WorkerFeingClient;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
-
-import java.util.HashMap;
-import java.util.Map;
 
 @Service
 public class PaymentService {
 
-    @Value("${hr-worker.host}")
-    private String workerHost;
-
     @Autowired
-    private RestTemplate restTemplate;
+    private WorkerFeingClient workerFeingClient;
 
     public Payment getPayment(long workerId, int days){
-        Map<String, String> uriVariables = new HashMap<>();
-        uriVariables.put("id", String.valueOf(workerId));
-
-        Worker worker = restTemplate.getForObject(workerHost + "/workers/{id}", Worker.class, uriVariables);
+        Worker worker = workerFeingClient.findById(workerId).getBody();
         return new Payment(worker.getName(), worker.getDailyIncome(), days);
     }
 }
