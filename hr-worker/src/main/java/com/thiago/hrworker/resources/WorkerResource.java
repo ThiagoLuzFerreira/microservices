8da756611d2c11a105fc36d8b7d395e166756c2e
Thiago Ferreira <tlrferreira83@gmail.com>
2022-08-25 16:28:17 -0300

    db config server

--- hr-worker/src/main/java/com/thiago/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/thiago/hrworker/resources/WorkerResource.java
@@ -44,11 +44,11 @@ public class WorkerResource {
     @GetMapping("/{id}")
     public ResponseEntity<Worker> findById(@PathVariable Long id){
 
-        try {
-            Thread.sleep(3000L);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
+//        try {
+//            Thread.sleep(3000L);
+//        } catch (InterruptedException e) {
+//            throw new RuntimeException(e);
+//        }
 
         logger.info("PORT = " + env.getProperty("local.server.port"));
 
